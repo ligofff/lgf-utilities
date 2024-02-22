5f9177827c06848f39f530d7c874b90bd37ec4ef
ligofff <liga@ligofff.ru>
2024-02-22 09:55:51 +0300

    Package init

---
+++ Runtime/HierarchyPath.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public static class HierarchyPath
+{
+    public static string GetHierarchyPath(Transform o)
+    {
+        var path = "";
+        while (o != null)
+        {
+            path = o.name + "/" + path;
+            o = o.parent;
+        }
+
+        return path;
+    }
+        
+    public static string GetHierarchyPath(GameObject o)
+    {
+        return GetHierarchyPath(o.transform);
+    }
+}
